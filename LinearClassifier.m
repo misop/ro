52d4a036adc4634e82a51c71e6bfd86275c2a89a
zaeraal <zaeraal@gmail.com>
2013-04-29 21:30:26 +0300

    Pridany Linearny Klasifier, ale dako dobre nefunguje

---
+++ LinearClassifier.m
@@ -0,0 +1,47 @@
+function LinearClassifier( data_set, selected_column, threshold )
+%LINEARCLASSIFIER Skusi pouzit Linearny Klassifier na data
+%   data_set - matica dat ("shrooms"), bez stlpca ktory testujete
+%   selected_column - ten stlpec ktory sa snazite otestovat
+%   threshold - threshold po ktory akceptujeme chybu
+if(max(selected_column) > 2)
+    return;
+end;
+
+max_it = 100;
+[m, n] = size(data_set);
+vektor = ones(1, n);
+target = selected_column > 1;
+found = false;
+chyba = 0;
+
+for i=1:max_it
+    dots = (vektor * data_set.')';
+    dots = dots > 0;
+    error = abs(dots - target);
+    chyba = (sum(error) / m) * 100;
+    if(chyba < threshold)
+        found = true;
+        break;
+    else
+        k = ceil(1 + (sum(error)-1).*rand);
+        idx = 0;
+        for j = 1:m
+            if(error(j) > 0)
+                idx = idx + 1;
+                if(idx == k)
+                    vektor = vektor + data_set(j,:);
+                end;
+            end;
+        end;
+    end;
+end;
+
+if(found == true)
+    disp('nasiel sa oddelujuci vektor');
+    disp(chyba);
+    disp(vektor);
+else
+    disp('nenasiel sa oddelujuci vektor');
+    disp(chyba);
+    disp(vektor);
+end;
