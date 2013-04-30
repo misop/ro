0eca63a6b571156a4b65951620dbfa43c8db0eba
misop <michal.piovarci@gmail.com>
2013-04-30 08:48:08 +0200

    vysvetlujuci comment

--- testBayes.m
+++ testBayes.m
@@ -8,6 +8,7 @@ targetTrain = c;
 targetTest = c;
 
 %treba najprv vyhdoit columny co hodia chybu :-)
+%ak nezostane ziaden tak sa neda rozdelit
 
 O1 = NaiveBayes.fit(train, targetTrain);
 C1 = O1.predict(test);
